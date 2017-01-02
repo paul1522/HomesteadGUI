a321f09bc839052c4a635f8ac8c693cd826101e8
Paul Generes <paul.generes@gmail.com>
2017-01-01 23:17:53 -0600

    Feature: Edit **folders** and **sites** sections of `Homestead.yaml` in the configuration dialog.

--- src/configure.pas
+++ src/configure.pas
@@ -5,14 +5,22 @@ unit configure;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
-  ExtCtrls, EditBtn, StdCtrls, ActnList, ComCtrls, data, homestead;
+  Classes, SysUtils, memds, db, FileUtil, Forms, Controls, Graphics, Dialogs,
+  ExtCtrls, EditBtn, StdCtrls, ActnList, ComCtrls, DBGrids, Menus, DbCtrls,
+  data, homestead;
 
 type
 
   { TConfigDialog }
 
   TConfigDialog = class(TForm)
+    SiteSource: TDataSource;
+    SiteNavigator: TDBNavigator;
+    SiteGrid: TDBGrid;
+    SiteData: TMemDataset;
+    FolderSource: TDataSource;
+    FolderNavigator: TDBNavigator;
+    FolderGrid: TDBGrid;
     EditHomesteadYaml: TAction;
     EditHosts: TAction;
     ActionList1: TActionList;
@@ -24,6 +32,7 @@ type
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
+    FolderData: TMemDataset;
     PageControl1: TPageControl;
     SaveButton: TButton;
     CancelButton: TButton;
@@ -35,11 +44,17 @@ type
     procedure EditHomesteadYamlExecute(Sender: TObject);
     procedure EditHostsExecute(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FolderDataNewRecord(DataSet: TDataSet);
     procedure SaveButtonClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
     { private declarations }
     YamlFileName : String;
+    PathsLoaded : Boolean;
+    procedure LoadFolders;
+    procedure LoadSites;
+    procedure SaveFolders;
+    procedure SaveSites;
   public
     { public declarations }
   end;
@@ -58,7 +73,14 @@ begin
   HomesteadDirSelector.Text := Global.HomesteadDir;
   VagrantCmdSelector.Text := Global.VagrantCmd;
   TextEditorCmdSelector.Text := Global.TextEditorCmd;
-  HostsFileEditorCmdSelector.Text := Global.HostsFileEditorCmd
+  HostsFileEditorCmdSelector.Text := Global.HostsFileEditorCmd;
+  TabSheet2.TabVisible := Global.YamlIsJson;
+  TabSheet3.TabVisible := Global.YamlIsJson;
+  if Global.YamlIsJson and not PathsLoaded then begin
+    LoadFolders;
+    LoadSites;
+    PathsLoaded := TRUE
+  end
 end;
 
 procedure TConfigDialog.SaveButtonClick(Sender: TObject);
@@ -67,12 +89,16 @@ begin
   Global.VagrantCmd := VagrantCmdSelector.Text;
   Global.TextEditorCmd := TextEditorCmdSelector.Text;
   Global.HostsFileEditorCmd := HostsFileEditorCmdSelector.Text;
-  Global.SaveIniFile
+  Global.Save(FolderData, SiteData)
 end;
 
 procedure TConfigDialog.EditHomesteadYamlExecute(Sender: TObject);
 begin
-  AHomestead.DetatchProcess(Global.TextEditorCmd, YamlFileName)
+  try
+    AHomestead.DetatchProcess(Global.TextEditorCmd, YamlFileName)
+  except
+    ShowMessage(Global.TextEditorCmd + ' failed to launch.')
+  end
 end;
 
 procedure TConfigDialog.EditHostsExecute(Sender: TObject);
@@ -86,7 +112,34 @@ end;
 
 procedure TConfigDialog.FormCreate(Sender: TObject);
 begin
-  YamlFileName := GetEnvironmentVariable('USERPROFILE') + '/.homestead/Homestead.yaml'
+  YamlFileName := GetEnvironmentVariable('USERPROFILE') + '/.homestead/Homestead.yaml';
+  PageControl1.ActivePageIndex := 0;
+  PathsLoaded := FALSE
+end;
+
+procedure TConfigDialog.FolderDataNewRecord(DataSet: TDataSet);
+begin
+  DataSet.FieldByName('Enabled').Value := true
+end;
+
+procedure TConfigDialog.LoadFolders;
+begin
+  Global.LoadFolders(FolderData)
+end;
+
+procedure TConfigDialog.LoadSites;
+begin
+  Global.LoadSites(SiteData)
+end;
+
+procedure TConfigDialog.SaveFolders;
+begin
+
+end;
+
+procedure TConfigDialog.SaveSites;
+begin
+
 end;
 
 end.
